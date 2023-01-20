0fbf8739a7b10e35d599889437aec76db8271350
jp <30583333+jpcorona@users.noreply.github.com>
2023-01-20 12:29:40 -0300

    Update app.py

--- app.py
+++ app.py
@@ -10,66 +10,63 @@ df = pd.read_csv("https://raw.githubusercontent.com/jpcorona/demoAgrosuper/main/
 
 
 st.set_page_config(
-    page_title = 'Panel en tiempo real modelo data science',
+    page_title = 'Real-Time Data Science Dashboard',
     page_icon = '✅',
     layout = 'wide'
 )
 
-# titulo panel
+# dashboard title
 
-st.title("Panel en tiempo real - Modelo Data Science")
+st.title("Real-Time / Live Data Science Dashboard")
 
-# filtros
+# top-level filters 
 
-area_filter = st.selectbox("Seleccione una area", pd.unique(df['area']))
+job_filter = st.selectbox("Select the Job", pd.unique(df['job']))
 
 
-# crear un contenedor
-
+# creating a single-element container.
 placeholder = st.empty()
 
-# filtro del dataframe
+# dataframe filter 
 
-df = df[df['area']==area_filter]
+df = df[df['job']==job_filter]
 
-# NRTP / simulación de muestras en tiempo real
+# near real-time / live feed simulation 
 
 for seconds in range(200):
 #while True: 
     
-    df['rutas_atrasadas'] = df['ruta'] * np.random.choice(range(1,12)) 
-    df['nuevo_balance'] = df['balance'] * np.random.choice(range(1,5)) 
+    df['age_new'] = df['age'] * np.random.choice(range(1,5))
+    df['balance_new'] = df['balance'] * np.random.choice(range(1,5))
 
-    # creando el KPI
+    # creating KPIs 
+    avg_age = np.mean(df['age_new']) 
 
-    avg_rutas= np.mean(df['rutas_atrasadas'])  #sacamos la media
-
-    contador_casados = int(df[(df["estado_maquina"]=='buena')]['estado_maquina'].count() + np.random.choice(range(1,30)))
+    count_married = int(df[(df["marital"]=='married')]['marital'].count() + np.random.choice(range(1,30)))
     
-    balance = np.mean(df['nuevo_balance']) #sacamos la media
+    balance = np.mean(df['balance_new'])
 
     with placeholder.container():
-        # crear 3 columnas para los kpi
+        # create three columns
         kpi1, kpi2, kpi3 = st.columns(3)
 
-        # complete esas tres columnas con las respectivas métricas o KPI 
-        kpi1.metric(label="KPI 1 🐖", value=round(avg_rutas), delta= round(avg_rutas) - 10)
-        kpi2.metric(label="KPI 2 🐔", value= int(contador_casados), delta= - 10 + contador_casados)
-        kpi3.metric(label="KPI 3 🦃", value= f"$ {round(balance,2)} ", delta= - round(balance/contador_casados) * 100)
+        # fill in those three columns with respective metrics or KPIs 
+        kpi1.metric(label="Age ⏳", value=round(avg_age), delta= round(avg_age) - 10)
+        kpi2.metric(label="Married Count 💍", value= int(count_married), delta= - 10 + count_married)
+        kpi3.metric(label="A/C Balance ＄", value= f"$ {round(balance,2)} ", delta= - round(balance/count_married) * 100)
 
-        # crear 2 columnas para graficos
+        # create two columns for charts 
 
         fig_col1, fig_col2 = st.columns(2)
         with fig_col1:
-            st.markdown("### Gráfico 1")
-            fig = px.density_heatmap(data_frame=df, y = 'rutas_atrasadas', x = 'estado_maquina')
+            st.markdown("### First Chart")
+            fig = px.density_heatmap(data_frame=df, y = 'age_new', x = 'marital')
             st.write(fig)
         with fig_col2:
-            st.markdown("### Gráfico 2")
-            fig2 = px.histogram(data_frame = df, x = 'rutas_atrasadas')
+            st.markdown("### Second Chart")
+            fig2 = px.histogram(data_frame = df, x = 'age_new')
             st.write(fig2)
-        st.markdown("### Vista detallada de los datos")
+        st.markdown("### Detailed Data View")
         st.dataframe(df)
         time.sleep(1)
     #placeholder.empty()
-
